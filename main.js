ba535721e638b182292a80619af9d169869da8df
Logzilla6 <84390269+Logzilla6@users.noreply.github.com>
2024-10-22 07:25:56 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         KhanHack
+// @namespace    https://greasyfork.org/users/783447
+// @version      5.5
+// @description  Khan Academy Answer Hack
+// @author       Logzilla6 - IlyTobias - Illusions
+// @match        https://*.khanacademy.org/*
+// @icon         https://i.ibb.co/K5g1KMq/Untitled-drawing-3.png
+// ==/UserScript==
+
+//ALL FOLLOWING CODE IS UNDER THE KHANHACK TRADEMARK. UNAUTHORIZED DISTRIBUTION CAN RESULT IN LEGAL ACTION
